bfd0d184cb8f0dc6d110784ab840a313b64e87c2
babaiyu <bayuputra9a@gmail.com>
2022-01-16 21:33:31 +0700

    Implements IVehicle interface

--- src/vehicle/Motor.java
+++ src/vehicle/Motor.java
@@ -1,14 +1,19 @@
 package vehicle;
 
-public class Motor extends Vehicle {
+public class Motor extends Vehicle implements IVehicle {
 	private Integer totalHelmet;
 
 	public Motor(String type, String brand, String name, String licenseNumber, Integer topSpeed, Integer gasCap,
 			Integer wheel, String typeVehicle) {
 		super(type, brand, name, licenseNumber, topSpeed, gasCap, wheel, typeVehicle);
 	}
-	
-	public void showMotor() {
+
+	public void setTotalHelmet(Integer totalHelmet) {
+		this.totalHelmet = totalHelmet;
+	}
+
+	@Override
+	public void show() {
 		System.out.println("Type => " + super.type);
 		System.out.println("Brand =>" + super.brand);
 		System.out.println("Name =>" + super.name);
@@ -20,8 +25,14 @@ public class Motor extends Vehicle {
 		System.out.println("Total Helmet =>" + this.totalHelmet);
 	}
 
-	public void setTotalHelmet(Integer totalHelmet) {
-		this.totalHelmet = totalHelmet;
+	@Override
+	public String getType() {
+		return super.type;
+	}
+
+	@Override
+	public String getName() {
+		return super.name;
 	}
 
 }
