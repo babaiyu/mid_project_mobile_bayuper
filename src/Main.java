c9223a44aa8317c24b879fa2928195911cbe8e9f
babaiyu <bayuputra9a@gmail.com>
2022-01-17 22:08:03 +0700

    Integrated input vehicle with validation helpers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import helpers.Helpers;
 import vehicle.*;
 
 public class Main {
@@ -8,6 +9,7 @@ public class Main {
 
 	// List of Menu
 	private static void menu() {
+		System.out.println();
 		System.out.println();
 		System.out.println("======= PT. MUSANG =======");
 		System.out.println("Choose Menu: ");
@@ -51,31 +53,61 @@ public class Main {
 	}
 
 	// Add Vehicle
-	private static void menu2() {
+	private static Boolean menu2() {
 		Scanner inputVehicle = new Scanner(System.in);
+		Helpers helpers = new Helpers();
+
 		System.out.println();
 		System.out.println("======= Add Vehicle =======");
 
 		System.out.print("Input Type [Car | Motorcycle]: ");
 		String iType = inputVehicle.nextLine();
+		if (helpers.checkType(iType) == false) {
+			System.out.println("WRONG TYPE!");
+			return false;
+		}
 
 		System.out.print("Input Brand [ >=5 ]: ");
 		String iBrand = inputVehicle.nextLine();
+		if (helpers.checkBrandorName(iBrand) == false) {
+			System.out.println("INPUT BRAND MUST >= 5 CHARACTERS!");
+			return false;
+		}
 
 		System.out.print("Input Name [ >= 5 ]: ");
 		String iName = inputVehicle.nextLine();
+		if (helpers.checkBrandorName(iName) == false) {
+			System.out.println("INPUT NAME MUST >= 5 CHARACTERS!");
+			return false;
+		}
 
 		System.out.print("Input License: ");
 		String iLicenseNumber = inputVehicle.nextLine();
+		if (helpers.checkLicenseNumber(iLicenseNumber) == false) {
+			return true;
+		}
 
 		System.out.print("Input Top Speed [100 <= topSpeed <= 250 ]: ");
 		Integer iTopSpeed = inputVehicle.nextInt();
+		if (helpers.checkTopSpeed(iTopSpeed) == false) {
+			System.out.println("INPUT TOP SPEED MUST BETWEEN 100 - 250! ");
+			return false;
+		}
 
 		System.out.print("Input Gas Capacity [ 30 <= gasCap <= 60 ]: ");
 		Integer iGasCap = inputVehicle.nextInt();
+		if (helpers.checkGasCapacity(iGasCap) == false) {
+			System.out.println("INPUT GAS CAPACITY MUST BETWEEN 30 - 60!");
+			return false;
+		}
 
 		System.out.print(iType.equals("Car") ? "Input Wheel [ 4 <= Wheel <= 6 ]: " : "Input Wheel [ 2 <= Wheel <= 3 ]: ");
 		Integer iWheel = inputVehicle.nextInt();
+		if (helpers.checkWheel(iWheel, iType) == false) {
+			String errorWheel = iType.equals("Car") ? "4 - 6" : "2 - 3";
+			System.out.println("INPUT WHEEL MUST BETWEEN " + errorWheel);
+			return false;
+		}
 
 		// Fix skipped input
 		inputVehicle.nextLine();
@@ -83,10 +115,19 @@ public class Main {
 		System.out.print(iType.equals("Car") ? "Input Type Vehicle [ SUV | Supercar | Minivan ]: "
 				: "Input Type Vehicle [ Automatic | Manual ]: ");
 		String iTypeVehicle = inputVehicle.nextLine();
+		if (helpers.checkTypeVehicle(iTypeVehicle, iType) == false) {
+			String errorTypeVehicle = iType.equals("Car") ? "SUV or Supercar or Minivan" : "Automatic or Manual";
+			System.out.println("INPUT TYPE VEHICLE MUST " + errorTypeVehicle);
+			return false;
+		}
 
 		if (iType.equals("Car")) {
 			System.out.print("Input Entertainment System [ >= 1 ]: ");
 			Integer iEntertainment = inputVehicle.nextInt();
+			if (helpers.checkEntertainmentSystem(iEntertainment) == false) {
+				System.out.println("INPUT ENTERTAINMENT SYSTEM MUST >= 1");
+				return false;
+			}
 
 			Car car = new Car(iType, iBrand, iName, iLicenseNumber, iTopSpeed, iGasCap, iWheel, iTypeVehicle);
 			car.setEntertainmentSystem(iEntertainment);
@@ -95,6 +136,10 @@ public class Main {
 		} else if (iType.equals("Motorcycle")) {
 			System.out.print("Input Helmet [ >= 1 ]: ");
 			Integer iHelmet = inputVehicle.nextInt();
+			if (helpers.checkHelmet(iHelmet) == false) {
+				System.out.println("INPUT HELMET MUST >= 1");
+				return false;
+			}
 
 			Motor motor = new Motor(iType, iBrand, iName, iLicenseNumber, iTopSpeed, iGasCap, iWheel, iTypeVehicle);
 			motor.setTotalHelmet(iHelmet);
@@ -102,7 +147,14 @@ public class Main {
 			list.add(motor);
 		} else {
 			System.out.println("Wrong input Type");
+			return false;
 		}
+
+		inputVehicle.nextLine();
+		System.out.print("ENTER to return");
+		inputVehicle.nextLine();
+
+		return true;
 	}
 
 	// Main Method
